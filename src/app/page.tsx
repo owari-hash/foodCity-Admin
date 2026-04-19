88f4c45488dc77062e6c6a3975d4c70193b07edc
jrrsenpai <jrrsenpai@gmail.com>
2026-04-20 05:06:59 +0800

    z

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 import { ADMIN_BASE_PATH } from "@/lib/adminBasePath";
 
 export default function Home() {
-  redirect(`${ADMIN_BASE_PATH}/dashboard/`);
+  redirect(`${ADMIN_BASE_PATH}/login/`);
 }
